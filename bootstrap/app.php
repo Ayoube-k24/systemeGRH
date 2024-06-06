3d57d8cd04e854a5bf92560a9bc2bb129dae830d
Ayoube <ayoube.kribich@gmail.com>
2024-06-06 22:29:12 +0100

    Multi Authentication commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,10 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\SuperAdmin;
+use App\Http\Middleware\Admin;
+use App\Http\Middleware\User;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware -> alias([
+            'superadmin' => SuperAdmin::class,
+            'admin' => Admin::class,
+            'user'=>User::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
